0f82d541c2ba716691d8565ceb208d73ee933f35
Jenna <112132967+Jen1w3n@users.noreply.github.com>
2024-01-19 16:02:06 +0200

    Added a foreground image to the game over screen.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,43 @@
-import { Text } from 'react-native';
+import { View, Image, Text, StyleSheet } from "react-native";
 
-function GameOverScreen () {
-    return <Text>Game over loser!!!</Text>
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
+
+function GameOverScreen() {
+  return (
+    <View style={styles.rootContainer}>
+      <Title>Game over LOSER!!!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/hahacat.png")}
+        />
+      </View>
+      <Text>Your phone needed x rounds to guess the numbe y.</Text>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 7,
+    borderColor: Colors.primary500,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
